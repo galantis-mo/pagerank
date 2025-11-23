0d62d5bcaddfaf2601a5d5c1513d2e062b783cfa
Monique <77052330+galantis-mo@users.noreply.github.com>
2025-11-23 15:25:03 +0000

    Update data_fetcher, remove some parameters and save only 10% of compressed data

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -4,7 +4,8 @@ DOCUMENTATION = """Python code to parse english wikilinks 'backup' from 2022-12-
     | 2. When {-tl TRIPLE} is used, {-l LIMIT} should indicate the number of triples considered from the original file.
 
     # Output format
-    | Outputs should be saved in ~/pagerank/data/uris.txt and ~/pagerank/data/children.txt being respectively the list of all 
+    | If -csv or --to_csv used, then only one file is created with parent; children
+    | Otherwise should be saved in ~/pagerank/data/uris.txt and ~/pagerank/data/children.txt being respectively the list of all 
     | subjects and for each of them, all of their children.
     | Example :     uris.txt   |   children.txt
     |                <a>       |    <b> <c> <d>
@@ -20,7 +21,47 @@ DOCUMENTATION = """Python code to parse english wikilinks 'backup' from 2022-12-
     | The unzipped file will be saved in ~/pagerank/data/original.bz2 if needed.
 
 """
-import argparse, textwrap
+import argparse, textwrap, math
+
+def save_to_csv(decompressor, csv_path:str, limit:int, comp_space):
+    # Open the file in write mode
+    file_csv = open(csv_path, "w")
+    file_csv.write('"parent";"child')
+
+    # Decompressed data and tracking current parent
+    data = ""
+    current_space_taken = 0
+    current_parent = ""
+
+    while (not decompressor.needs_input) and (current_space_taken < limit):
+        data += decompressor.decompress(b'', max_length=256).decode('utf-8', "backslashreplace")
+
+        # Reading all triples in buffer
+        for triple in data.split('\n'):
+
+            # Check for complete triple
+            if triple.endswith(' .'):
+                elements = triple.split(' ')
+
+                if elements[0] != current_parent:
+                    file_csv.write('"\n"{}";"'.format(elements[0]))
+                    current_parent = elements[0]
+                    current_space_taken += comp_space(elements[0], False)
+                else:
+                    file_csv.write(' ')
+                
+                file_csv.write(elements[2])
+
+                current_space_taken += comp_space(elements[2], True)
+            
+            # Uncomplete triple
+            else:
+                data = triple
+
+    file_csv.write('"')
+    file_csv.close()
+    return current_space_taken
+
 
 def DataFetcher(limit:int, is_limit_size:bool, save_comp:bool):
     import sys, os.path, bz2    # To manage files
@@ -28,99 +69,65 @@ def DataFetcher(limit:int, is_limit_size:bool, save_comp:bool):
     import subprocess           # To execute linux commands
 
     # Paths to important files
-    dir_path = os.path.dirname(os.path.abspath(__file__))
-    data_dir_path = os.path.join(os.path.dirname(dir_path), 'data')
+    dir_path = os.path.dirname(__file__)
+    original_path = os.path.join(dir_path, '..', 'data', 'original.bz2')
+    output_csv_path = os.path.join(dir_path, '..', 'data', 'wikilinks.csv')
 
-    original_path = os.path.join(data_dir_path, 'original.bz2')
-    childrens_path = os.path.join(data_dir_path, 'children.txt')
-    uris_path = os.path.join(data_dir_path, 'uris.txt')
+    print("Will try to save to", output_csv_path)
 
     # Fetch compressed data
     if os.path.exists(original_path):
-        print("Original bz2 file already existent in pagerank/data...")
+        print("Original bz2 found file already existent in pagerank/data...")
         with open(original_path, 'rb') as f:
             compressed_data = f.read()
+
     else:
         request = urllib.request.Request("https://downloads.dbpedia.org/repo/dbpedia/generic/wikilinks/2022.12.01/wikilinks_lang=en.ttl.bz2")
         try:
             with urllib.request.urlopen(request) as data:
                 print("Sending request to \"https://downloads.dbpedia.org/repo/dbpedia/generic/wikilinks/2022.12.01/wikilinks_lang=en.ttl.bz2\" ...")
-
                 compressed_data = data.read()
-                
-                # Save the original bz2 file
-                if save_comp:
-                    with open(original_path, 'wb') as f:
-                        f.write(compressed_data)
+
         except urllib.error.URLError as e:
             print(e.reason)
+            exit(1)
 
-    # Open files in write mode
-    file_children = open(childrens_path, "w")
-    file_uris = open(uris_path, "w")
-    file_uris.write('\n')
+        # Using only 10% of the original file
+        compressed_data = compressed_data[:math.ceil(len(compressed_data)*0.1)]
+        if save_comp:
+            with open(original_path, 'wb') as f:
+                f.write(compressed_data)
 
-    current_space_taken = 0
 
     # Decompressing the zipped data
-    print("Decompressing data from the original file")
+    print("Starting decompressing data from the original file")
     decompressor = bz2.BZ2Decompressor()
-    unzipped_data = decompressor.decompress(compressed_data, max_length=256).decode('utf-8', "backslashreplace")
-    current_id = ""
+    decompressor.decompress(compressed_data, max_length=0).decode('utf-8', "backslashreplace")
 
-    print(f"Limit set to {limit} MB = {limit*1000000} B")
-    limit = 1000000
-    while (current_space_taken < limit):
-        unzipped_data += decompressor.decompress(b'', max_length=256).decode('utf-8', "backslashreplace")
-        
-        for triple in unzipped_data.split('\n'):       
-            if triple.endswith(' .'):   # Triples follows format : <sub> <pred> <obj> .
-                elms = triple.split(' ')
-
-                # New subject
-                if elms[0] != current_id:
-                    subject = elms[0]+'\n'
-                    file_uris.write(subject)
-                    file_children.write('\n')
-
-                    current_id = elms[0]
-                    current_space_taken += len(subject) if is_limit_size else 0
-                
-                # New child
-                if len(elms) < 2:
-                    print(elms)
-                child = elms[-2] + ' '
-                file_children.write(child)
-                current_space_taken += len(child) if is_limit_size else 1
-
-            else:
-                # Not complete triple
-                unzipped_data = triple
-    
-    # Final report
-    file_children.close()
-    file_uris.close()
-
-    if decompressor.needs_input:
-        print("All triples extracted...")
-    else:
-        print("Only some of triples were extracted...")
-
+    ## Limit size computation
     if is_limit_size:
-        print(f"Output of size {current_space_taken} MB.")
+        comp_space = lambda elm,_: len(elm)
     else:
-        print(f"{current_space_taken} triples considered during decompression.")
+        comp_space = lambda _,is_child: 1 if is_child else 0
+
+    ## Output file
+    current_space_taken = save_to_csv(decompressor, output_csv_path, limit, comp_space)
+
+    ## Final report
+    print("All triples extracted...") if decompressor.needs_input else print("Only some of triples were extracted...")    
+    print(f"Output of size {current_space_taken/1000000} MB.") if is_limit_size else print(f"{current_space_taken} triples considered during decompression.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-                    prog='data_fetcher.py',
+                    prog='python data_fetcher.py',
                     description=textwrap.dedent(DOCUMENTATION),
                     formatter_class=argparse.RawDescriptionHelpFormatter,
                     epilog='Monique RIMBERT (monique.rimbert@etu.univ-nantes.fr)')
 
     parser.add_argument('-tl', '--type_limit', type=str, choices=['SIZE', 'TRIPLE'], default="SIZE", help="The type of limit considered use 'SIZE' in MB and 'TRIPLE' as a int.")
-    parser.add_argument('-l', '--limit', type=int, default=1000, help='The size limit in type_limit of file.')
+    parser.add_argument('-l', '--limit', type=int, default=1, help='The size limit in type_limit of file.')
     parser.add_argument('-S', '--save_comp', action="store_true" , help="Saves the zip in data with to path ~/data/original.bz2")
+
     
     args = parser.parse_args()   
-    DataFetcher(args.limit, args.type_limit == "SIZE", args.save_comp)
+    DataFetcher(args.limit*1000000, args.type_limit == "SIZE", args.save_comp)
