6c90f17476160464ff4f723c3367f3efbcc1b6e2
Monique <77052330+galantis-mo@users.noreply.github.com>
2025-12-03 14:18:41 +0000

    Update data_fetcher to use google cloud api

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -1,27 +1,12 @@
-DOCUMENTATION = """Python code to parse english wikilinks 'backup' from 2022-12-01
-    # Input format
-    | 1. When {-l LIMIT} should indicate the size of uris.txt and children.txt alltogether in mega bytes.
+import os
+import sys
 
-    # Output format
-    | A parent and the list of its children
-    | Example :     uris.txt   |   children.txt
-    |                <a>       |    <b> <c> <d>
-    |                <b>       |    <d> <e>
+PROJECT_ID = os.environ['PROJECT_ID']
+BUCKET_NAME = os.environ['BUCKET']
 
-    # Possible error during execution 
-    | Sometimes `bz2 module is not available error` happens. In this case :
-    | > cp /usr/lib/<PYTHON_VERSION>/lib-dynload/_bz2.cpython-<PYTHON_VERSION>-x86_64-linux-gnu.so /usr/local/lib/<PYTHON_VERSION>/
-    | should suffice to fix it.
-
-    # Implementation comment
-    | Beware that opening solely this url will result in redirection, hence use urllib.request.Request to process redirections.
-    | The unzipped file will be saved in ~/pagerank/data/original.bz2 if needed.
-"""
-import argparse, textwrap
-
-def save_to_csv(decompressor, csv_path:str, limit:int):
+def save_to_csv(decompressor, csv_path, limit:int):
     # Open the file in write mode
-    file_csv = open(csv_path, "w")
+    file_csv = csv_path.open(mode="w", encoding="utf-8")
     file_csv.write('"";"')
 
     # Decompressed data and tracking current parent
@@ -58,46 +43,44 @@ def save_to_csv(decompressor, csv_path:str, limit:int):
     file_csv.close()
     return current_space_taken
 
-def DataFetcher(limit:int, output_dir:str):
-    import os.path, bz2    # To manage files
+def DataFetcher(limit:int):
+    from google.cloud import storage
+    import bz2    # To manage files
     import urllib.request       # To fetch file
 
     # Fetch compressed data
     request = urllib.request.Request("https://downloads.dbpedia.org/repo/dbpedia/generic/wikilinks/2022.12.01/wikilinks_lang=en.ttl.bz2")
-
     try:
         with urllib.request.urlopen(request) as data:
             print("Sending request to \"https://downloads.dbpedia.org/repo/dbpedia/generic/wikilinks/2022.12.01/wikilinks_lang=en.ttl.bz2\" ...")
             compressed_data = data.read(180000000)
+
     except urllib.error.URLError as e:
         print("Fail to request or read wikilinks_lang=en.ttl.bz2:", e.reason)
         exit(1)
 
     # Paths to important files
-    output_csv_path = os.path.join(output_dir, 'wikilinks.csv')
-    print("Will try to save to", output_csv_path)
+    storage_client = storage.Client(PROJECT_ID)
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob("data/wikilinks.csv")
+    
+    print("Will try to save to", BUCKET_NAME)
 
     # Decompressing the zipped data
     print("Starting decompressing data from the original file")
     decompressor = bz2.BZ2Decompressor()
     decompressor.decompress(compressed_data, max_length=0).decode('utf-8', "backslashreplace")
 
-    ## Output file
-    current_space_taken = save_to_csv(decompressor, output_csv_path, limit)
+    current_space_taken = save_to_csv(decompressor, blob, limit)
 
     ## Final report
     print("All triples extracted...") if decompressor.needs_input else print("Only some of triples were extracted...")    
     print(f"Output of size {current_space_taken/1000000} MB.")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                    prog='python data_fetcher.py',
-                    description=textwrap.dedent(DOCUMENTATION),
-                    formatter_class=argparse.RawDescriptionHelpFormatter,
-                    epilog='')
-
-    parser.add_argument('--limit', type=int, default=1, help='The size limit of file in MB.')
-    parser.add_argument('--output_dir', help="The path of the output directory")
-
-    args = parser.parse_args()   
-    DataFetcher(args.limit*1000000, args.output_dir)
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+        print("Donner la limite du fichier en entier !")
+        exit(2)
+    
+    # Appel de la méthode
+    DataFetcher(int(sys.argv[2])*1000000)
